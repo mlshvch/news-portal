06fc6c95a60dddeda44b198a29354b6609d111e4
mlshvch <malashevichdev@gmail.com>
2021-12-14 17:44:18 +0300

    Configure simplecov Gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,13 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
-#
+
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/' # for rspec
+end
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
