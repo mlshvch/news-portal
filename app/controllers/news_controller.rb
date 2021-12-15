7c69b0cb1c2523c2ebbbdb75ec21a28fe7353bed
mlshvch <malashevichdev@gmail.com>
2021-12-15 13:35:47 +0300

    Fix controller

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,19 +1,24 @@
 class NewsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def index
     @articles = News.all
   end
 
-  def show
-    @article = News.find(params[:id])
+  def show!
+    find_article
+    # if article nil then redirect to the main page
+    unless @article 
+      flash[:notice] = "Article not found"
+      redirect_to root_path
+    end
   end
 
   def new
     @article = News.new
   end
 
-  def create
+  def create!
     @article = News.new(news_params)
 
     if @article.save
@@ -23,12 +28,12 @@ class NewsController < ApplicationController
     end
   end
 
-  def edit
-    @article = News.find(params[:id])
+  def edit!
+    find_article
   end
 
-  def update
-    @article = News.find(params[:id])
+  def update!
+    find
 
     if @article.update(news_params)
       redirect_to @article
@@ -37,14 +42,18 @@ class NewsController < ApplicationController
     end
   end
 
-  def destroy
-    @article = News.find(params[:id])
+  def destroy!
+    find_article
     @article.destroy
 
     redirect_to root_path
   end
 
   private
+
+  def find_article 
+    @article = News.find(params[:id])
+  end
 
   def news_params
     params.require(:news).permit(:title, :body)
