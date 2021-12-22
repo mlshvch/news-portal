fa145c6be1f249af67ae9c548e3cd1975e017172
mlshvch <malashevichdev@gmail.com>
2021-12-22 20:24:39 +0300

    Add authentication request for news_articles actions

--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -1,5 +1,5 @@
 class NewsArticlesController < ApplicationController
-  before_action :set_news_article, only: %i[show edit update destroy]
+  before_action :set_news_article, only: %i[show edit update destroy], :authenticate_user!
 
   # GET /news_articles
   def index
