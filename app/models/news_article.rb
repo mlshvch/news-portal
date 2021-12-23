ecd7592f9d958fcb879f80adfd7471066240d4f5
mlshvch <malashevichdev@gmail.com>
2021-12-23 13:04:20 +0300

    Connect user and news_article

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class NewsArticle < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
--- app/policies/news_article_policy.rb
+++ app/policies/news_article_policy.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 class NewsArticlePolicy < ApplicationPolicy
+    def index?
+        true
+      end
+    
+      def create?
+        user.present?
+      end
+    
+      def update?
+        return true if user.present?
+      end
+    
+      def destroy?
+        return true if user.present?
+      end
+    
+      private
+    
+      def article
+        record
+      end    
 end
