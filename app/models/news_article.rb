00f1244942f6b413440db3fe7a458af30d2502cc
mlshvch <malashevichdev@gmail.com>
2021-12-23 19:33:18 +0300

    Connect users and articles

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,4 +1,5 @@
 class NewsArticle < ApplicationRecord
+  belongs_to :user, class_name: "user", foreign_key: "user_id"
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :news_article, class_name: "news_article", foreign_key: "news_article_id"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
