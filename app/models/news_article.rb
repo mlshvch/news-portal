ff5421113f467321351ccc867c7adae6723f0df1
mlshvch <malashevichdev@gmail.com>
2021-12-24 10:32:56 +0300

    Launch Devise generator

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class NewsArticle < ApplicationRecord
-  belongs_to :user, class_name: 'user', foreign_key: 'user_id'
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
