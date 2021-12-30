dfed9dace0bc279141f6b0431a5e94c960ea320b
mlshvch <malashevichdev@gmail.com>
2021-12-30 15:19:45 +0300

    Rollback schema and delete redundant migration

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,6 @@ ActiveRecord::Schema.define(version: 2021_12_24_125006) do
     t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_news_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,5 +37,4 @@ ActiveRecord::Schema.define(version: 2021_12_24_125006) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "news_articles", "users"
 end
