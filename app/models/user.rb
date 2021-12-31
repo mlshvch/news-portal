0eaaa6b4b78e17d00f740f454db9e4079644a759
mlshvch <malashevichdev@gmail.com>
2021-12-31 18:30:31 +0300

    Add User email and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@
 
 class User < ApplicationRecord
   has_many :news_articles
+
+  validates :email, presence: true
+  validates :password, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
