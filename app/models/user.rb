99e1da767d80a9e12899cc207ee4992239928d3e
mlshvch <malashevichdev@gmail.com>
2021-12-31 18:56:58 +0300

    Add password length validation to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :news_articles
 
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
