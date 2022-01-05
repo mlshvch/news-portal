2c1e36ab3c13b70dab8ade4dbf431f13966bc8c9
mlshvch <malashevichdev@gmail.com>
2022-01-05 16:02:38 +0300

    Fix policies with requirements. Delete redundant verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include Pundit
-  after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -11,7 +10,7 @@ class ApplicationController < ActionController::Base
   private
 
   def user_not_authorized
-    flash[:alert] = 'You are not authorized to perform this action'
+    flash[:notice] = 'You are not authorized to perform this action'
     redirect_to news_articles_path
   end
 end
