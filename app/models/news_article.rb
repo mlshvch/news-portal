1f847104916db44038542986f363945f68e26ea0
mlshvch <malashevichdev@gmail.com>
2022-01-06 22:06:43 +0300

    Add events and transitions of state to NewsArticle

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -5,4 +5,25 @@ class NewsArticle < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  enum state: %i[not_active active published archived]
+
+  include AASM
+
+  aasm column: :state, enum: true do
+    state :not_active, initial: true
+    state :active, :published, :archived
+
+    event :approve do
+      transitions from: :not_active, to: :active
+    end
+
+    event :publish do
+      transitions from: :active, to: :published
+    end
+
+    event :archive do
+      transitions from: :published, to: :archived
+    end
+  end
 end
