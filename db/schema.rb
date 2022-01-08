25349a2cae5f737157a367b193ed5d5fe14dee4f
mlshvch <malashevichdev@gmail.com>
2022-01-08 13:01:47 +0300

    Add 'Important' Flag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_08_070734) do
+ActiveRecord::Schema.define(version: 2022_01_08_095558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2022_01_08_070734) do
     t.text "description"
     t.integer "access_rights", default: 0, null: false
     t.datetime "published_at"
+    t.boolean "important", default: false, null: false
     t.index ["user_id"], name: "index_news_articles_on_user_id"
   end
 
