56b6fddf9aafd94fef5acdca9ba5bae824720703
mlshvch <malashevichdev@gmail.com>
2022-01-09 23:13:22 +0300

    Create Comments Controller. Update routes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class CommentsController < ActionController::Base
+  before_action :set_comment
+
+  def create
+    @comment = Comment.new(news_article_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to news_article_url(news_article_id), notice: 'Comment was successfully created.' }
+      else
+        format.html do
+          redirect_to news_article_url(news_article_id), notice: @comment.errors.full_messages.join(',').to_s
+        end
+      end
+    end
+  end
+
+  def destroy
+    @comment = Comment.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to news_article_url(news_article_id), notice: 'Comment was successfully deleted.' }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @comment.update(news_article_params)
+        format.html { redirect_to news_article_url(news_article_id), notice: 'News article was successfully updated.' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def news_article_id
+    comment_params[:news_article_id]
+  end
+
+  def set_comment; end
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :news_article_id).merge(user: current_user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'news_articles#index'
 
-  resources :news_articles
-
   resources :news_articles do
+    resources :comments
     member do
       put 'approve', to: 'news_articles#approve_news', as: :approve
       put 'publish', to: 'news_articles#publish_news', as: :publish
