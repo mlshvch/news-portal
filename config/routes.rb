e00d5ea26ab9873bf16ffba58774f4b95996baf0
mlshvch <malashevichdev@gmail.com>
2022-01-10 05:27:05 +0300

    Add rss route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'news_articles#index'
 
+  get 'feeds/rss', to: 'feeds#rss_index', format: 'rss'
+
   resources :news_articles do
     resources :comments
     member do
