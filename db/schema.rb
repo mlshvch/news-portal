8352d4479fac540e29dac614364ddb5a8740cae2
mlshvch <malashevichdev@gmail.com>
2022-01-10 10:06:14 +0300

    Add personal data to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_09_194531) do
+ActiveRecord::Schema.define(version: 2022_01_10_070314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,10 @@ ActiveRecord::Schema.define(version: 2022_01_09_194531) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "name"
+    t.string "surname"
+    t.string "nickname"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
